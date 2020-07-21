139b8c5c8b9bd56a7a67205a04ea19c5dbfec29c
mlazzell17@gmail.com <MicheSi>
2020-07-21 14:39:28 -0700

    using Arrays & Collections

--- arrays/Main.java
+++ arrays/Main.java
@@ -31,6 +31,34 @@ public class Main {
         for (int i = 0; i < ournums.length; i++) {
             System.out.println("Array Element " + i + " = " + ournums[i]);
         }
+
+        System.out.println();
+        System.out.println("*** Another Way Example ***");
+        // initialize an array of strings using a list of values
+        // the size of the array is set as the number of items in the list
+        String[] cars = {"Volvo", "BMW", " Ford", "Mazda"};
+
+        System.out.println(cars.length);
+        System.out.println(cars[0]);
+
+        // change 2nd element in array
+        cars[1] = "Honda";
+
+        // print array's pointer
+        System.out.println(cars);
+
+        System.out.println("Before sort Element index 2 " + cars[2]);
+
+        // sort array in ascending order using Arrays class
+        Arrays.sort(cars);
+        System.out.println("After sort Element index 2 " + cars[2]);
+
+        // sort array in descending order with Arrays & Collections
+        Arrays.sort(cars, Collections.reverseOrder());
+
+        System.out.println();
+        System.out.println("Array in descending order");
+        System.out.println(Arrays.toString(cars));
     }
 }
 
