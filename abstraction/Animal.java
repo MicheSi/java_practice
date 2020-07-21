9e3601d4a92be898c9d2cff38a98c28d9d7ae9a2
mlazzell17@gmail.com <MicheSi>
2020-07-21 13:12:29 -0700

    created abstraction folder and animal class

---
+++ abstraction/Animal.java
@@ -0,0 +1,15 @@
+package abstraction;
+
+abstract class Animal {
+    int weight = 50;
+
+    abstract void animalSound();
+
+    void sleep() {
+        System.out.println("ZZZ");
+    }
+
+    int getWeight() {
+        return weight;
+    }
+}
